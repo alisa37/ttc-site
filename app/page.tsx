45f485cb32d3572bf6ba2bf14e33e25860a60572
alisa37 <a.kuga37@gmail.com>
2026-06-06 23:24:02 +0900

    3 full page test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,64 @@
+import Script from "next/script";
+
 export default function Home() {
   return (
-    <main>
-      <div>PAGE 1</div>
-      <div>PAGE 2</div>
-      <div>PAGE 3</div>
-    </main>
+    <>
+      <Script
+        id="organization-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "SportsOrganization",
+            name: "Tokyo Tennis Club",
+            url: "https://tokyo-tennis-club.com",
+            logo: "https://tokyo-tennis-club.com/favicon.ico",
+            sameAs: ["https://www.meetup.com/tokyo-tennis-club/"],
+          }),
+        }}
+      />
+
+      <main>
+        <section
+          style={{
+            minHeight: "100vh",
+            background: "#111",
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+          }}
+        >
+          PAGE 1
+        </section>
+
+        <section
+          style={{
+            minHeight: "100vh",
+            background: "#fff",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+          }}
+        >
+          PAGE 2
+        </section>
+
+        <section
+          style={{
+            minHeight: "100vh",
+            background: "#eee",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "3rem",
+          }}
+        >
+          PAGE 3
+        </section>
+      </main>
+    </>
   );
 }
