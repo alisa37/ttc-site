cf3c38e3806053981c24518dad5e7321b1871135
alisa37 <a.kuga37@gmail.com>
2026-06-30 18:14:15 +0900

    Update analytics tracking

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,6 +4,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://tokyo-tennis-club.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://tokyo-tennis-club.com/tennis-in-tokyo",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
